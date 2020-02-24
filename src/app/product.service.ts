510a9b150ac723ff07a770cc9c2fdacfb10163db
cbakaj <cbakaj36@gmail.com>
2020-02-24 15:01:58 -0500

    change1

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -5,11 +5,12 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
-  _albumUrl: '../assets/album.json';
+  private _albumUrl = '../assets/album.json';
   constructor(private _http: Http) { }
 
   getAlbum(id: number) {
-      return this._http.get(this._albumUrl).map(response => response.json())
+      return this._http.get(this._albumUrl).map((response) =>
+      response.json());
   }
 
 }
